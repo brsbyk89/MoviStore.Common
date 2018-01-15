93d4dc6621703be5fe37456f735d4fa4f6323a7d
brsbyk89 <barisbiyik@hotmail.com>
2018-01-15 22:31:11 +0300

    Caching

---
+++ MovieStore.Core.Caching/HttpCacheProvider.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using MovieStore.Core.Caching.Contract;
+using System.Runtime.Caching;
+
+namespace MovieStore.Core.Caching
+{
+    public class HttpCacheProvider : ICacheProvider
+    {
+        public HttpCacheProvider()
+        {
+
+        }
+
+        public void Set<T>(string key, T value)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            memoryCache.Add(key, value,null);
+        }
+
+        public void Set<T>(string key, T value, TimeSpan timeout)
+        {
+
+            MemoryCache memoryCache = MemoryCache.Default;
+            memoryCache.Add(key, value, Convert.ToDateTime(timeout));
+        }
+
+        public T Get<T>(string key)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            var result = memoryCache.Get(key);
+
+            return (T)Convert.ChangeType(result, typeof(T));
+        }
+
+        public bool Remove(string key)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            if (memoryCache.Contains(key))
+            {
+                memoryCache.Remove(key);
+            }
+
+            if (!IsInCache(key))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsInCache(string key)
+        {
+            bool isInCache = false;
+
+            MemoryCache memoryCache = MemoryCache.Default;
+            if (memoryCache.Contains(key))
+            {
+                isInCache = true;
+            }
+
+            return isInCache;
+        }
+    }
+}
---
+++ MovieStore.Core.Caching/RedisConfigurationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+
+namespace MovieStore.Core.Caching
+{
+    public class RedisConfigurationManager
+    {
+        #region Constants
+
+        private const string SectionName = "RedisConfiguration";
+
+        public static RedisConfigurationSection Config
+        {
+            get
+            {
+                return (RedisConfigurationSection)ConfigurationManager.GetSection(SectionName);
+            }
+        }
+
+        #endregion
+    }
+}
